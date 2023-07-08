0ae5df9884d0db2d7225e74f66498fefd0f30fa9
Jonathas Silva <jonathas_alves5@hotmail.com>
2023-07-08 18:52:23 -0300

    feat: Colocar post

--- src/Components/PostModel/index.jsx
+++ src/Components/PostModel/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import styles from './PostModel.module.css'
 import './Post.css'
+import Posts from 'Components/Posts'
+import { useParams } from 'react-router-dom';
 
 export default function PostModel({ coverPhoto, title, children }) {
+  const params = useParams();
+
   return (
+
     <article className={styles.postModelContainer}>
       <div 
         className={styles.coverPhoto}
@@ -17,7 +22,13 @@ export default function PostModel({ coverPhoto, title, children }) {
 
       <div className={styles.postContentContainer}>
         {children}
+        <h2>Outros posts que voce pode gostar:</h2>
+        <Posts
+          size="4"
+          id={params.id}
+        />
       </div>
+
     </article>
   )
 }
--- src/Components/Posts/index.jsx
+++ src/Components/Posts/index.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Posts.module.css'
 import posts from 'json/posts.json'
 import PostCard from 'Components/Posts/PostCard'
 
-export default function Posts() {
+export default function Posts( { size, id}) {
+  const [filteredPosts, setFilteredPosts] = useState([]);
+
+  useEffect(() => {
+    let filtered = posts;
+
+    if(size) {
+      const maxPosts = Math.min(parseInt(size), posts.length);
+      filtered = posts.filter((post) => post.id.toString() !== id).slice(0, maxPosts);
+    }
+
+    setFilteredPosts(filtered);
+  }, [size, id])
+
   return (
     <ul className={styles.posts}>
-        {posts.map((post) => (
+        {filteredPosts.map((post) => (
           <li key={post.id}>
             <PostCard 
               post={post}
